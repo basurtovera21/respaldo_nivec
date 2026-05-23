28f14493f1240a4c644825d37bcb96bcfb87c280
DENGy <dengymacias2017@gmail.com>
2026-05-23 18:50:06 -0500

    Enums

--- clases/enums/estado_de_periodo.py
+++ clases/enums/estado_de_periodo.py
@@ -1,1 +1,9 @@
-#Dengy
+# Periodo de nivelación
+from enum import Enum
+
+
+class EstadoDePeriodo(Enum):
+    PLANIFICACION = "Planificación"
+    EN_CURSO = "En curso"
+    EVALUACION = "Evaluación"
+    CERRADO = "Cerrado"
